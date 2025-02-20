0aae222f2998d53694ec6d84de1f3fa2c9fc7222
Jackihyun <pkhjack2325@gmail.com>
2025-02-20 15:00:18 +0900

    feat: eslint 설정함.

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import eslint from "@eslint/js";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintPluginReact from "eslint-plugin-react";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist", "node_modules"] },
+  eslint.config.recommended,
+  ...tseslint.configs.recommended,
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react: eslintPluginReact,
+      "react-hooks": eslintPluginReactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
+      semi: ["error", "never"],
+      indent: ["error", 2],
+
+      // React 관련 규칙
+      ...eslintPluginReact.configs.flat.recommended.rules,
+      ...eslintPluginReact.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "react/jsx-uses-vars": "error",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
     },
-  },
-)
+  }
+);
