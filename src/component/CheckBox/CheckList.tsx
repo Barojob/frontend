42d3cd8c7335386542cd658d22cd0c21cfb01c4d
Jackihyun <pkhjack2325@gmail.com>
2025-02-25 00:58:31 +0900

    feat: 전체 동의해야만 넘어가지는 오류 수정.

--- src/component/CheckBox/CheckList.tsx
+++ src/component/CheckBox/CheckList.tsx
@@ -7,24 +7,16 @@ type CheckItem = {
   id: number;
   label: string;
   checked: boolean;
-<<<<<<< HEAD
   required: boolean;
-=======
->>>>>>> bea64eb (feat: 회원가입 페이지 구조 세팅 및 첫스텝 페이지 구현 완료.)
 };
 
 type Props = {
   className?: string;
-<<<<<<< HEAD
   onAllCheckedChange: (allRequiredChecked: boolean) => void;
-=======
-  onAllCheckedChange: (allChecked: boolean) => void;
->>>>>>> bea64eb (feat: 회원가입 페이지 구조 세팅 및 첫스텝 페이지 구현 완료.)
 };
 
 const CheckList: React.FC<Props> = ({ className, onAllCheckedChange }) => {
   const [items, setItems] = useState<CheckItem[]>([
-<<<<<<< HEAD
     {
       id: 1,
       label: "바로잡 서비스 이용약관 동의",
@@ -67,20 +59,8 @@ const CheckList: React.FC<Props> = ({ className, onAllCheckedChange }) => {
   useEffect(() => {
     onAllCheckedChange(isRequiredChecked);
   }, [isRequiredChecked, onAllCheckedChange]);
-=======
-    { id: 1, label: "[필수] 바로잡 서비스 이용약관 동의", checked: false },
-    { id: 2, label: "[필수] 개인정보 수집 및 이용 동의", checked: false },
-    { id: 3, label: "[선택] 개인정보 수집 및 이용 동의", checked: false },
-    { id: 4, label: "[선택] 개인정보 제3자 제공 동의", checked: false },
-    { id: 5, label: "[선택] 마케팅 정보 수신 동의", checked: false },
-  ]);
 
-  const isAllChecked = items.every((item) => item.checked);
-
-  useEffect(() => {
-    onAllCheckedChange(isAllChecked);
-  }, [isAllChecked, onAllCheckedChange]);
->>>>>>> bea64eb (feat: 회원가입 페이지 구조 세팅 및 첫스텝 페이지 구현 완료.)
+  // 다음 버튼 활성화는 필수 항목만 체크되었는지를 기준으로 합니다.
 
   const toggleAll = () => {
     setItems(items.map((item) => ({ ...item, checked: !isAllChecked })));
@@ -104,29 +84,16 @@ const CheckList: React.FC<Props> = ({ className, onAllCheckedChange }) => {
       />
 
       {/* 개별 체크박스들 */}
-<<<<<<< HEAD
-      <div className="flex flex-col">
-        {items.map((item) => (
-          <button className="mb-3.5" onClick={() => toggleItem(item.id)}>
-            <CheckBox
-              key={item.id}
-              isChecked={item.checked}
-              onToggle={() => toggleItem(item.id)}
-              label={
-                item.required ? `[필수] ${item.label}` : `[선택] ${item.label}`
-              }
-            />
-          </button>
-=======
       <div className="gap-3 flex flex-col">
         {items.map((item) => (
           <CheckBox
             key={item.id}
             isChecked={item.checked}
             onToggle={() => toggleItem(item.id)}
-            label={item.label}
+            label={
+              item.required ? `[필수] ${item.label}` : `[선택] ${item.label}`
+            }
           />
->>>>>>> bea64eb (feat: 회원가입 페이지 구조 세팅 및 첫스텝 페이지 구현 완료.)
         ))}
       </div>
     </div>
