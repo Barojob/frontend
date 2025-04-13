94655a86c12d71084f1a7f9fde8fed841522e0e5
jeheecheon <jeheecheon@gmail.com>
2025-04-13 17:04:52 +0900

    chore: tailwindcss 옵션 추가 세팅

--- prettier.config.js
+++ prettier.config.js
@@ -5,5 +5,18 @@ export default {
   tabWidth: 2,
   semi: true,
   endOfLine: "lf",
+  tailwindConfig: "./tailwind.config.js",
+  tailwindStylesheet: ".src/index.css",
+  /**
+   * @description
+   * Tailwind CSS 클래스 사용하는 곳에 정렬이 필요한 Attribute, 이곳에 넣어주세요.
+   */
+  tailwindAttributes: [
+    "labelClassName",
+    "buttonClassName",
+    "imageClassName",
+    "listClassName",
+    "itemClassName",
+  ],
   plugins: ["prettier-plugin-tailwindcss"],
 };
