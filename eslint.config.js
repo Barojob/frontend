db1d801c93d0a4e845eb0d4b4ee7d507f0c2fb09
jeheecheon <jeheecheon@gmail.com>
2025-04-13 16:45:26 +0900

    chore: eslint 세팅 flat config 스타일로 정리

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,15 @@
-import js from "@eslint/js";
 import eslint from "@eslint/js";
-import eslintPluginReactHooks from "eslint-plugin-react-hooks";
-import eslintPluginReact from "eslint-plugin-react";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import reactRefreshPlugin from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import storybookPlugin from "eslint-plugin-storybook";
 
 export default tseslint.config(
-  { ignores: ["dist", "node_modules"] },
-  eslint.config.recommended,
-  ...tseslint.configs.recommended,
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      react: eslintPluginReact,
-      "react-hooks": eslintPluginReactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      semi: ["error", "never"],
-      indent: ["error", 2],
-
-      // React 관련 규칙
-      ...eslintPluginReact.configs.flat.recommended.rules,
-      ...eslintPluginReact.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "react/jsx-uses-vars": "error",
-      "react/react-in-jsx-scope": "off",
-      "react-hooks/rules-of-hooks": "error",
-    },
-  }
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactHooksPlugin.configs["recommended-latest"],
+  reactRefreshPlugin.configs.recommended,
+  storybookPlugin.configs["flat/recommended"],
 );
