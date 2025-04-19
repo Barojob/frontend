a5ffce35a7c365040cd3d04799c91d556120f187
jeheecheon <jeheecheon@gmail.com>
2025-04-20 00:40:16 +0900

    refactor: encapsulate root rendering logic in a function for better structure

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { assert } from "./utils/assert";
 
-const rootElement = document.getElementById("root");
-assert(!!rootElement, "Root element not found");
+const renderRoot = () => {
+  const rootElement = document.getElementById("root");
+  assert(!!rootElement, "Root element not found");
 
-createRoot(rootElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+};
+renderRoot();
